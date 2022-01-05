b3effd842c2818958453a6fb75972e9e2b3f1488
Novan R. Kuncoro <enerka4ig@gmail.com>
2022-01-05 09:04:44 +0700

    Finished task for JS Learning Day 1

---
+++ javascript/day-1/index.js
@@ -0,0 +1,19 @@
+// Task: Create a function that has parameters name of book & quantity of the book, then display sentence information of the product.
+// Example: There are 5 Harry Potter books left in stock
+
+function printBookStock(name, quantity) {
+  if (!quantity) {
+    return console.log(`There are no books left for ${name}`)
+  }
+
+  let toBe = quantity > 1 ? 'are' : 'is'
+  let articles = quantity > 1 ? quantity : 'a single of'
+  let noun = quantity > 1 ? 'books' : 'book'
+  let sentence = `There ${toBe} ${articles} ${name} ${noun} left in the stock`
+
+  console.log(sentence)
+}
+
+printBookStock("The Psychology of Money", 1)
+printBookStock("Harry Potter", 5)
+
