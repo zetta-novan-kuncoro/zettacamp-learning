cceeb5f4ad8f23e0921497f47e6a5fae06a201a4
Novan R. Kuncoro <enerka4ig@gmail.com>
2022-01-06 09:58:46 +0700

    Add a case where the quantity is 0

--- javascript/day-1/index.js
+++ javascript/day-1/index.js
@@ -6,14 +6,16 @@ function printBookStock(name, quantity) {
     return console.log(`There are no books left for ${name}`)
   }
 
-  let toBe = quantity > 1 ? 'are' : 'is'
-  let articles = quantity > 1 ? quantity : 'a single of'
-  let noun = quantity > 1 ? 'books' : 'book'
-  let sentence = `There ${toBe} ${articles} ${name} ${noun} left in the stock`
+  const isPlural = quantity > 1
+
+  let toBe = isPlural ? 'are' : 'is'
+  let articles = isPlural ? quantity : 'a single'
+  let noun = isPlural ? 'books' : 'book'
+  let sentence = `There ${toBe} ${articles} of ${name} ${noun} left in the stock`
 
   console.log(sentence)
 }
 
 printBookStock("The Psychology of Money", 1) // There is a single of The Psychology of Money book left in the stock
 printBookStock("Harry Potter", 5) // There are 5 Harry Potter books left in the stock
-
+printBookStock("Lord of the Ring", 0) // There are no books left for Lord of the Ring
