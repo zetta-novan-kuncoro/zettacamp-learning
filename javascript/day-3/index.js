77c934ce2582c71daf0171dfd6c6f15769a4277e
Novan R. Kuncoro <enerka4ig@gmail.com>
2022-01-07 10:17:13 +0700

    Provide comments

--- javascript/day-3/index.js
+++ javascript/day-3/index.js
@@ -1,13 +1,16 @@
 // Try creating a function that has parameters name of book & quantity of the book as an object, then display sentence if book can be purchased or not.
 
+// Dummy stocks data. Pretend it lives in a database :P
 const stocks = [
   { id: 1, bookID: 1234, quantity: 25 },
   { id: 2, bookID: 2345, quantity: 0 },
   { id: 3, bookID: 3456, quantity: 1 },
 ]
-  
+ 
+// A function to get the stock information for the book from the stocks data.
 const getStock = bookID => stocks.find(stock => stock.bookID === bookID)
 
+// A function to buy book that accepts a book object as the parameter.
 const buyBook = ({ id, name, quantity }) => {
   if (!quantity) {
     return console.error('    Tell me how many books you want to buy')
@@ -15,7 +18,7 @@ const buyBook = ({ id, name, quantity }) => {
 
   const stock = getStock(id)
 
-  if (!stock.quantity) {
+  if (!stock?.quantity) {
     return console.error(`    There is no stock currently available for your purchase of ${name}`)
   }
 
@@ -33,10 +36,13 @@ const buyBook = ({ id, name, quantity }) => {
   console.log(sentence)
 }
 
+// A list of my actual wishlist of books that I want to buy
 const wishlist = [
   { id: 1234, name: 'The Psychology of Money', quantity: 1 },
   { id: 2345, name: 'The Subtle Art of Not Giving a F*ck', quantity: 1 },
   { id: 3456, name: 'Filosofi Teras', quantity: 2 },
+  { id: 4567, name: 'Atomic Habits' },
 ]
 
+// Iterate the wishlist and buy the book
 for (const book of wishlist) buyBook(book)
